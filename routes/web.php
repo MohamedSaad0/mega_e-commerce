0d8b3306ea45c7a8986101a899211d593659def8
Your Name <you@example.com>
2022-04-12 12:50:32 +0200

    add sb-admin-2 theme

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\PasswordResetLinkController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,12 +15,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+Route::get('/', function () {
+    return view("auth.login");
+});
+
+Route::get("/logout", [AuthenticatedSessionController::class, "destroy"])->name("auth.logout");
+Route::get("/forgot-password", [PasswordResetLinkController::class, "create"])->name("auth.forgot-password");
