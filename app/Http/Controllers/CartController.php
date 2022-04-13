1c4f6ab8fbf98589e3fd7130bad7ad615b959159
artscss <artscss@gmail.com>
2022-04-13 23:56:42 +0200

    taha

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -7,32 +7,14 @@ use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         //
     }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         //
     }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         //
